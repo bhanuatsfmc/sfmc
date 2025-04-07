f99aaf2da96467af9b51b63875781539f891f914
bhanuatsfmc <bhantatsfdc@gmail.com>
2025-04-08 01:25:10 +0200

    Update sendlogs.js

--- sendlogs.js
+++ sendlogs.js
@@ -1,37 +1,78 @@
+<!-- Inside the existing <script runat="server"> block -->
 <script runat="server">
 Platform.Load("core", "1");
 
 try {
+  var startDateStr = Request.GetQueryStringParameter("startDate");
+  var endDateStr = Request.GetQueryStringParameter("endDate");
+  var currentPage = parseInt(Request.GetQueryStringParameter("page") || "1", 10);
+  var pageSize = 10;
+
+  if (startDateStr && endDateStr) {
+    var startDate = startDateStr + "T00:00:00";
+    var endDate = endDateStr + "T23:59:59";
+
     var ws = new Script.Util.WSProxy();
     var props = ["ID", "EmailName", "EmailSubject", "SendDate", "FromName", "FromAddress"];
-
     var filter = {
-        Property: "SendDate",
-        SimpleOperator: "between",
-        Value: ["2024-03-01T00:00:00", "2025-03-31T23:59:59"]
+      Property: "SendDate",
+      SimpleOperator: "between",
+      Value: [startDate, endDate]
     };
 
-    var result = ws.retrieve("Send", props, filter);
-    var sends = result.Results;
+    var options = {
+      BatchSize: 2500
+    };
 
-    if (sends && sends.length > 0) {
-        Write("<table border='1' cellpadding='5'><tr><th>SendID</th><th>Email Name</th><th>Subject</th><th>Send Date</th><th>From</th></tr>");
-        for (var i = 0; i < sends.length; i++) {
-            var s = sends[i];
-            Write("<tr>");
-            Write("<td>" + s.ID + "</td>");
-            Write("<td>" + s.EmailName + "</td>");
-            Write("<td>" + s.EmailSubject + "</td>");
-            Write("<td>" + s.SendDate + "</td>");
-            Write("<td>" + s.FromName + " (" + s.FromAddress + ")</td>");
-            Write("</tr>");
+    var retrieved = ws.retrieve("Send", props, filter, options);
+    var allResults = retrieved.Results || [];
+    var totalRecords = allResults.length;
+
+    // Manual pagination
+    var totalPages = Math.ceil(totalRecords / pageSize);
+    var startIndex = (currentPage - 1) * pageSize;
+    var pagedResults = allResults.slice(startIndex, startIndex + pageSize);
+
+    Write("<div id='ajaxContent'>");
+
+    if (pagedResults.length > 0) {
+      Write("<table class='table table-bordered table-striped'>");
+      Write("<thead class='table-light'><tr>");
+      Write("<th>Send ID</th><th>Email Name</th><th>Subject</th><th>Send Date</th><th>From Name</th><th>From Address</th>");
+      Write("</tr></thead><tbody>");
+
+      for (var i = 0; i < pagedResults.length; i++) {
+        var send = pagedResults[i];
+        Write("<tr>");
+        Write("<td>" + send.ID + "</td>");
+        Write("<td>" + send.EmailName + "</td>");
+        Write("<td>" + send.EmailSubject + "</td>");
+        Write("<td>" + send.SendDate + "</td>");
+        Write("<td>" + send.FromName + "</td>");
+        Write("<td>" + send.FromAddress + "</td>");
+        Write("</tr>");
+      }
+
+      Write("</tbody></table>");
+
+      // Pagination
+      if (totalPages > 1) {
+        Write("<nav><ul class='pagination'>");
+        for (var p = 1; p <= totalPages; p++) {
+          var active = (p == currentPage) ? " active" : "";
+          Write("<li class='page-item" + active + "'><a href='#' class='page-link' data-page='" + p + "'>" + p + "</a></li>");
         }
-        Write("</table>");
+        Write("</ul></nav>");
+      }
+
     } else {
-        Write("<p>No send data found in the given date range.</p>");
+      Write("<div class='alert alert-warning text-center'>No sends found for this date range.</div>");
     }
 
+    Write("</div>");
+  }
+
 } catch (e) {
-    Write("Error: " + Stringify(e));
+  Write("<div class='alert alert-danger'>Error: " + Stringify(e) + "</div>");
 }
 </script>
