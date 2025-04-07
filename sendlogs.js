f1d984ebb34568439ce83c92d619b323dd4cd0f9
bhanuatsfmc <bhantatsfdc@gmail.com>
2025-04-07 23:54:24 +0200

    Update sendlogs.js

--- sendlogs.js
+++ sendlogs.js
@@ -1,3 +1,4 @@
+<html>
 <head>
   <style>
     body, a, input, td, th {font-family:sans-serif; font-size:12px;}
@@ -9,103 +10,49 @@
 
   Platform.Load("core","1");
 
-  var debug = false;
-
-  try {
-
-    var prox = new Script.Util.WSProxy();
-    var mids = [536006542];
-
-    if (debug) {
-      Write("<br>mids: " + Stringify(mids));
+    // Configuration
+    var batchSize = 50;
+    var dateFormat = "yyyy-MM-dd";
+    
+    // Get request parameters
+  //  var startDate = Platform.Request.GetFormField("startDate");
+  //  var endDate = Platform.Request.GetFormField("endDate");
+  //  var currentPage = Number(Platform.Request.GetFormField("page") || 1);
+      var startDate = "2025-03-01";
+    var endDate = "2025-03-30";
+    var currentPage = 1;
+    var sdate = "SendDate";
+    // Initialize response variables
+    var sends = [];
+    var totalCount = 0;
+    var totalPages = 0;
+    
+    // Main processing
+    if (startDate && endDate) {
+        try {
+            var proxy = new Script.Util.WSProxy();
+            var cols = ["SendID", "EmailName", "SendDate", "Subject"];
+            var filter = {
+                Property: sdate,
+                SimpleOperator: "between",
+                Value: [startDate + "T00:00:00", endDate + "T23:59:59"]
+            };
+            
+            var options = {
+                BatchSize: batchSize,
+                Page: currentPage
+            };
+            
+            var result = prox.retrieve("DataExtensionObject[" + deName + "]", cols, filter, options);
+            sends = result.Results;
+            totalCount = result.TotalCount;
+            totalPages = Math.ceil(totalCount / batchSize);
+            
+        } catch (e) {
+  Platform.Response.Write("<br><br>e: " + Stringify(e));
+    
+        }
     }
-
-    Write("<table border='1'>");
-    Write("<tr>");
-    Write("<th>MID</th>");
-    Write("<th>Automation Name</th>");
-    Write("<th>Status</th>");
-    Write("</tr>");
-
-    for (i = 0; i < mids.length; i++) {
-
-      var mid = mids[i];
-      prox.setClientId({"ID": mid});
-      var obj = "Automation";
-
-      var cols = ["ProgramID","CustomerKey","Status","Name"];
-
-      // The only supported operators for the Automation object are: IN, EQUALS. Complex filter is not supported.
-      // https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/automation.htm
-
-      var filter = {
-          Property: "Status"
-          , SimpleOperator: "IN"
-          , Value: [-1,0,1,2,3,4,5,6,7,8]
-      };
-
-      var opts = null;
-      var props = null;
-
-      var moreObjs = true;
-      var reqID = null;
-
-      while (moreObjs) {
-
-          moreObjs = false;
-
-          var objs = reqID == null ? prox.retrieve(obj, cols, filter, opts, props) : prox.getNextBatch(obj, reqID);
-
-          if (objs != null) {
-
-              moreObjs = objs.HasMoreRows;
-              reqID = objs.RequestID;
-
-              for (var j = 0; j < objs.Results.length; j++) {
-
-                var automationObj = objs.Results[j];
-
-                var statusDsc = "";
-
-                switch (automationObj.Status) {
-                  case -1: statusDsc = "Error"; break;
-                  case  0: statusDsc = "Building Error"; break;
-                  case  1: statusDsc = "Building"; break;
-                  case  2: statusDsc = "Ready"; break;
-                  case  3: statusDsc = "Running"; break;
-                  case  4: statusDsc = "Paused"; break;
-                  case  5: statusDsc = "Stopped"; break;
-                  case  6: statusDsc = "Scheduled"; break;
-                  case  7: statusDsc = "Awaiting Trigger"; break;
-                  case  8: statusDsc = "Inactive Trigger"; break;
-                }
-
-                Write("<tr>");
-                Write("<td>" + mid + "</td>");
-                Write("<td>" + automationObj.Name + "</td>");
-                Write("<td>" + statusDsc + "</td>");
-                Write("</tr>");
-
-              }
-
-          }
-
-
-      }
-
-      prox.resetClientIds();
-
-    }
-
-    Write("</table>");
-
-  } catch (e) {
-
-    if (debug) {
-      Platform.Response.Write("<br><br>e: " + Stringify(e));
-    }
-
-  }
 </script>
 </body>
 </html>
